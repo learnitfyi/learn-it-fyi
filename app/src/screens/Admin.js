8e994bf5d2d2bf1501fcda9d69e36234f282296d
Josh Feinstein <joshua.scott.feinstein@gmail.com>
2020-06-06 19:12:25 -0400

    adds user login lifecycle method

--- app/src/screens/Admin.js
+++ app/src/screens/Admin.js
@@ -11,8 +11,36 @@ class Admin extends Component{
     super(props);
 
     this.state = {
-      loggedIn: false
-    }
+      loggedIn: null,
+      displayName: null,
+      email: null,
+      emailVerified: null,
+      photoURL: null,
+      isAnonymous: null,
+      uid: null,
+      providerData: null,
+    };
+  }
+
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        this.setState({
+          loggedIn: true,
+          displayName: user.displayName,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          photoURL: user.photoURL,
+          isAnonymous: user.isAnonymous,
+          uid: user.uid,
+          providerData: user.providerData
+        });
+        alert('user logged in');
+      } else {
+        alert('user logged out');
+        this.setState({ loggedIn: false });
+      }
+    }.bind(this));
   }
 
   render(){
@@ -20,7 +48,16 @@ class Admin extends Component{
       <div id="admin">
         <h1>Admin Dashboard</h1>
         {this.state.loggedIn
-          ? <AdminDashboard />
+          ? <AdminDashboard
+              loggedIn={this.state.loggedIn}
+              displayName={this.state.displayName}
+              email={this.state.email}
+              emailVerified={this.state.emailVerified}
+              photoURL={this.state.photoURL}
+              isAnonymous={this.state.isAnonymous}
+              uid={this.state.uid}
+              providerData={this.state.providerData}
+            />
           : <LogIn />
         }
       </div>
--- app/src/screens/AdminDashboard.js
+++ app/src/screens/AdminDashboard.js
@@ -5,7 +5,17 @@ import { Link } from "react-router-dom";
 class AdminDashboard extends Component{
    render(){
       return(
-         <div id="admin-dashboard">Logged In</div>
+         <div id="admin-dashboard">
+           <h1>Welcome, {this.props.displayName}</h1>
+           <p>{this.props.loggedIn}</p>
+           <p>{this.props.displayName}</p>
+           <p>{this.props.email}</p>
+           <p>{this.props.emailVerified}</p>
+           <p>{this.props.photoURL}</p>
+           <p>{this.props.isAnonymous}</p>
+           <p>{this.props.uid}</p>
+           <p>{this.props.providerData}</p>
+         </div>
       );
    }
 }
