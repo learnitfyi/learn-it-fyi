54cd0aea3c6d6110156ccfb582431402e41950e5
Josh Feinstein <joshua.scott.feinstein@gmail.com>
2020-06-07 01:43:30 -0400

    removes comments

--- app/src/reducers/user-reducer.js
+++ app/src/reducers/user-reducer.js
@@ -2,13 +2,11 @@
 const GET_LOGGED_IN_USER = 'GET_LOGGED_IN_USER'
 const LOG_IN_USER = 'LOG_IN_USER'
 const LOG_OUT_USER = 'LOG_OUT_USER'
-// const USER_SIGN_UP = 'USER_SIGN_UP'
 
 /* ACTION CREATORS */
 const gotUser = user => ({type: GET_LOGGED_IN_USER, user})
 const logInUser = user => ({type: LOG_IN_USER, user})
 const logOutUser = user => ({type: LOG_OUT_USER})
-// const userSignUp = user => ({type: USER_SIGN_UP})
 
 /* THUNK CREATORS */
 export const getUser = (item) => {
@@ -32,7 +30,6 @@ export const logIn = (email, password) => async dispatch => {
       user.loggedIn = true
     }
     dispatch(logInUser(user))
-    window.location.pathname = '/admin'
   } catch (err) {
     alert('username and/or password incorrect')
     console.error(err)
@@ -43,20 +40,10 @@ export const logOut = () => async dispatch => {
   try {
     await firebase.auth().signOut()
     dispatch(logOutUser())
-    dispatch(push('/log-in'));
   } catch (err) {
     console.error(err)
   }
 }
-
-// export const addSignedUpToStore = () => async dispatch => {
-//   try {
-//     dispatch(logInUser(userSignUp))
-//     dispatch(push('/admin'));
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
 
 /* REDUCER */
 export default function(user = { loggedIn: false }, action) {
